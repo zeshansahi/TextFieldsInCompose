efcd291f734419130070f77d7fb870cbc9b64e28
zeshansahi4u <zeshansahi4u@gmail.com>
2023-10-12 10:11:25 +0500

    coil image added

--- app/src/main/java/github/zeshansahi/composetextfields/MainActivity.kt
+++ app/src/main/java/github/zeshansahi/composetextfields/MainActivity.kt
@@ -1,20 +1,31 @@
 package github.zeshansahi.composetextfields
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import github.zeshansahi.composetextfields.ui.theme.ComposeTextFieldsTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,12 +53,36 @@ fun Greeting(name: String) {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         val context = LocalContext.current
+        ImageLoading()
         SimpleTextField()
         IconRichTextField()
         PasswordOutLinedTextField()
         CustomGoogleButton(onClick = {
             Toast.makeText(context, "Clicked", Toast.LENGTH_LONG).show()
         })
+    }
+}
+
+@Composable
+fun ImageLoading() {
+    Box(
+        modifier = Modifier
+            .width(100.dp)
+            .height(100.dp)
+    ) {
+        AsyncImage(
+            model = "https://avatars.githubusercontent.com/u/31881441?v=4",
+            contentDescription = "image-from-web",
+            onError = {
+                Log.e("TAG", "ImageLoading: $it")
+            },
+            placeholder = painterResource(R.drawable.screen_shot),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .height(100.dp)
+                .width(100.dp)
+                .clip(CircleShape)
+        )
     }
 }
 
